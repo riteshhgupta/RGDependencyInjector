f7a107c8931a55231cd9865cb482d15e21394670
Ritesh Gupta <rg.riteshh@gmail.com>
2018-11-13 22:47:14 +0530

    migrate to swift 4.2 🚀

--- RGDependencyInjector/AppDelegate.swift
+++ RGDependencyInjector/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	let window: UIWindow = UIWindow(frame: UIScreen.main.bounds)
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
 		// create the dependency
 		let fruits = ["apple", "banana", "orange"]
